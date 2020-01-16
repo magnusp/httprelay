bf222cf1dd9d6438316a1a31b31e26affd890a3f
Magnus Persson <magnus.e.persson@gmail.com>
2020-01-16 17:10:37 +0100

    Work on multicast

--- httprelay-server/src/main/java/se/fortnox/httprelay/server/DataPublisher.java
+++ httprelay-server/src/main/java/se/fortnox/httprelay/server/DataPublisher.java
@@ -3,30 +3,33 @@ package se.fortnox.httprelay.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
-import reactor.core.publisher.ConnectableFlux;
+import org.springframework.messaging.rsocket.MetadataExtractor;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-@Controller
+@RestController
 public class DataPublisher {
-    private final Logger log = LoggerFactory.getLogger(DataPublisher.class);
-    private final Flux<DataFrame> datastream;
+    private final Logger                      log     = LoggerFactory.getLogger(DataPublisher.class);
+    private final EmitterProcessor<DataFrame> datastream;
+	private       Queue<RSocketRequester>     clients = new ConcurrentLinkedQueue<>();
 
-    public DataPublisher() {
-		/*this.datastream = Flux.zip(
-				Flux.interval(Duration.ofSeconds(1)),
-				Flux.just("hej").repeat(),
-				(aLong, s) -> {
-					return new DataFrame(s + " " + aLong);
-				}
-		)
-				.doOnNext(dataFrame -> log.info(dataFrame.getValue()))
-		.publish();
-		this.datastream.connect();*/
-        this.datastream = Flux
+	public DataPublisher() {
+	    this.datastream = EmitterProcessor
+		    .<DataFrame>create();
+
+        /*this.datastream = Flux
                 .interval(Duration.ofSeconds(1))
                 .map(aLong -> new DataFrame("sequence " + aLong))
 				.onBackpressureLatest()
@@ -34,11 +37,32 @@ public class DataPublisher {
 					log.info("Publishing started");
 				})
                 .publish()
-		.refCount();
+		.refCount();*/
+    }
+
+    @GetMapping("/trigger")
+    public Mono<Void> trigger() {
+    	datastream.onNext(new DataFrame("Value: " + Date.from(Instant.now())));
+    	return Mono.empty();
+    }
+
+    @GetMapping("/inform")
+    public Mono<Void> inform() {
+		return Flux
+			.fromIterable(clients)
+			.flatMap(requester -> {
+				Map<String, String> metadata = Collections.singletonMap("route", "data.inform");
+
+				return requester
+					.metadata(metadata, MimeTypeUtils.parseMimeType("message/x.rsocket.routing.v0"))
+					.data(new DataFrame("some information")).send();
+			})
+			.then(Mono.empty());
     }
 
     @MessageMapping("data")
-    public Flux<DataFrame> getData() {
+    public Flux<DataFrame> getData(RSocketRequester requester) {
+    	clients.offer(requester);
         return datastream
 				.doOnSubscribe(subscription -> log.info("Subscriber connected"))
 				.doOnComplete(() -> log.info("Subscriber disconnected"));
