e1470eb6f96109be867cfc0ce62bcd88bb3fa82a
Magnus Persson <magnus.e.persson@gmail.com>
2020-01-29 20:16:34 +0100

    Use reactor-kafka instead of spring-kafka

--- httprelay-client/src/main/java/se/fortnox/httprelay/client/ClientController.java
+++ httprelay-client/src/main/java/se/fortnox/httprelay/client/ClientController.java
@@ -1,11 +1,16 @@
 package se.fortnox.httprelay.client;
 
 import io.rsocket.RSocket;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
 
 import javax.annotation.PreDestroy;
 import java.nio.charset.StandardCharsets;
@@ -13,7 +18,13 @@ import java.nio.charset.StandardCharsets;
 @Controller
 public class ClientController {
     private final Logger log = LoggerFactory.getLogger(ClientController.class);
+    private final KafkaSender<Integer, byte[]> kafkaSender;
     private RSocket rsocket;
+
+    @Autowired
+    public ClientController(KafkaSender<Integer, byte[]> kafkaSender) {
+        this.kafkaSender = kafkaSender;
+    }
 
     @PreDestroy
     void destroy() {
@@ -28,7 +39,16 @@ public class ClientController {
         return requester
                 .route("/events")
                 .retrieveFlux(byte[].class)
+                .flatMap(s -> {
+                    SenderRecord<Integer, byte[], Integer> message = SenderRecord.create(new ProducerRecord<>("hello", s), 1);
+                    return kafkaSender
+                            .send(Mono.just(message))
+                            .doOnNext(senderResult -> log.info(senderResult.toString()))
+                            .then(Mono.just(s));
+                })
                 .map(bytes -> new String(bytes, StandardCharsets.UTF_8))
+
                 .doOnNext(log::info);
+
     }
 }
---
+++ httprelay-client/src/main/java/se/fortnox/httprelay/client/ProducerConfiguration.java
@@ -0,0 +1,27 @@
+package se.fortnox.httprelay.client;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProducerConfiguration {
+    @Bean
+    KafkaSender<Integer, byte[]> kafkaSender() {
+        final Map<String, Object> producerProps = new HashMap<>();
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        final SenderOptions<Integer, byte[]> producerOptions = SenderOptions.create(producerProps);
+
+        return KafkaSender.create(producerOptions);
+    }
+}
