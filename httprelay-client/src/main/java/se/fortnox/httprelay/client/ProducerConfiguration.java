902f1027d1644f123110e99b4326d4f1524f6218
Magnus Persson <magnus.e.persson@gmail.com>
2020-01-30 09:09:31 +0100

    configuration

--- httprelay-client/src/main/java/se/fortnox/httprelay/client/ProducerConfiguration.java
+++ httprelay-client/src/main/java/se/fortnox/httprelay/client/ProducerConfiguration.java
@@ -3,6 +3,7 @@ package se.fortnox.httprelay.client;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.kafka.sender.KafkaSender;
@@ -13,12 +14,15 @@ import java.util.Map;
 
 @Configuration
 public class ProducerConfiguration {
+    @Value(value = "${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     KafkaSender<Integer, byte[]> kafkaSender() {
         final Map<String, Object> producerProps = new HashMap<>();
         producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
         final SenderOptions<Integer, byte[]> producerOptions = SenderOptions.create(producerProps);
 
